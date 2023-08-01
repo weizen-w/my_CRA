a3014f21088df49834f2972901f1c4dc3a87bdef
Wladimir Weizen <weizen.w@gmx.de>
2023-08-01 17:28:01 +0200

    feat: add styles

--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import User from '../UsersPage/types/User';
+import styles from './UserCard.module.css';
 
 export default function UserCard(): JSX.Element {
   const initialUser: User = {
@@ -35,29 +36,49 @@ export default function UserCard(): JSX.Element {
     loadUser();
   }, [userId]);
   return (
-    <div>
-      <p>
-        <Link to="..">Назад к списку товаров</Link>
+    <div className={styles.boxStyle}>
+      <p className={styles.btnStyle}>
+        <Link to="..">← Назад к списку пользователей</Link>
       </p>
       <img
-        style={{ height: '100px' }}
+        className={styles.imgStyle}
         src="https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png"
         alt={user.username}
       />
-      <h1>{user.name.lastname}, {user.name.firstname}</h1>
+      <h1 className={styles.nameStyle}>{user.name.lastname}, {user.name.firstname}</h1>
       <div>
-        <p>Username: {user.username}</p>
-        <p>Password: {user.password}</p>
-        <p>E-mail: {user.email}</p>
-        <p>Phone: {user.phone}</p>
-        <p>Address:</p>
-        <p>{user.address.zipcode} {user.address.city},</p>
-        <p>{user.address.street}, {user.address.number}</p>
-        <p>
-          Geolocation:
-          lat({user.address.geolocation.lat}),
-          long({user.address.geolocation.long})
-        </p>
+      <table className={styles.tableStyle}>
+        <tr>
+          <td className={styles.firstColumnStyle}>Username:</td>
+          <td className={styles.secondColumnStyle}>{user.username}</td>
+        </tr>
+        <tr>
+          <td className={styles.firstColumnStyle}>Password:</td>
+          <td className={styles.secondColumnStyle}>{user.password}</td>
+        </tr>
+        <tr>
+          <td className={styles.firstColumnStyle}>E-mail:</td>
+          <td className={styles.secondColumnStyle}>{user.email}</td>
+        </tr>
+        <tr>
+          <td className={styles.firstColumnStyle}>Phone:</td>
+          <td className={styles.secondColumnStyle}>{user.phone}</td>
+        </tr>
+        <tr>
+          <td className={styles.firstColumnStyle}>Address:</td>
+          <td className={styles.secondColumnStyle}>
+            {user.address.zipcode} {user.address.city},<br />
+            {user.address.street}, {user.address.number}
+          </td>
+        </tr>
+        <tr>
+          <td className={styles.firstColumnStyle}>Geolocation:</td>
+          <td className={styles.secondColumnStyle}>
+            lat({user.address.geolocation.lat}),
+            long({user.address.geolocation.long})
+          </td>
+        </tr>
+      </table>
       </div>
     </div>
   );
--- src/components/UsersPage/UsersPage.tsx
+++ src/components/UsersPage/UsersPage.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link, Outlet, useParams } from 'react-router-dom';
 import User from './types/User';
+import styles from './UsersPage.module.css';
 
 export default function UsersPage(): JSX.Element {
   const { userId } = useParams();
@@ -12,26 +13,35 @@ export default function UsersPage(): JSX.Element {
   }
   function getUsers(arr: User[]): JSX.Element {
     return (
-      <div>
+      <div className={styles.divStyle}>
         {arr.map((user: User) => (
-          <Link to={String(user.id)}>
-            <h1 key={user.id}>{user.name.lastname}, {user.name.firstname}</h1>
-            <img style={{ height: '100px' }} src="https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png" alt={user.username} />
-            <div>
-              <p>Username: {user.username}</p>
-              <p>Password: {user.password}</p>
-              <p>E-mail: {user.email}</p>
-              <p>Phone: {user.phone}</p>
-              <p>Address:</p>
-              <p>{user.address.zipcode} {user.address.city},</p>
-              <p>{user.address.street}, {user.address.number}</p>
-              <p>
-                Geolocation:
-                lat({user.address.geolocation.lat}),
-                long({user.address.geolocation.long})
-              </p>
+          <div className={styles.boxStyle}>
+            <Link to={String(user.id)} style={{ textDecoration: 'none' }}>
+              <h1 className={styles.nameStyle} key={user.id}>
+                {user.name.lastname}, {user.name.firstname}
+              </h1>
+            </Link>
+            <div className={styles.boxCardStyle}>
+              <img
+                className={styles.imgStyle}
+                src="https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png"
+                alt={user.username}
+              />
+              <div className={styles.datenBoxStyle}>
+                <p>Username: {user.username}</p>
+                <p>Password: {user.password}</p>
+                <p>E-mail: {user.email}</p>
+                <p>Phone: {user.phone}</p>
+                <p>Address:</p>
+                <p>{user.address.zipcode} {user.address.city},</p>
+                <p>{user.address.street}, {user.address.number}</p>
+                <p>
+                  Geolocation: lat({user.address.geolocation.lat}), long(
+                  {user.address.geolocation.long})
+                </p>
+              </div>
             </div>
-          </Link>
+          </div>
         ))}
       </div>
     );
